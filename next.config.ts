a088b8f0a8141fff827bb3a5bed77d4673bd9907
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-05-08 20:58:07 +0600

    Update GitHub Pages configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Enable static exports
-  basePath: '', // Remove the basePath since we're using a custom domain
+  basePath: '/thetahmeed.github.io',
   images: {
     unoptimized: true, // Required for static export
   },
