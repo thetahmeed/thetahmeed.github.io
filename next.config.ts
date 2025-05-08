297ce0bb29fdf2edc1768ec7fe4d52cee025b000
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2025-05-08 21:00:46 +0600

    Fix configuration for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Enable static exports
-  basePath: '/thetahmeed.github.io',
+  basePath: '',
   images: {
     unoptimized: true, // Required for static export
   },
